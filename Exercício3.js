b43c58ebd9184921fdfc126443ee6faaa6d9957f
Thomas-Stoianov <thomasgstoianov2@gmail.com>
2024-05-03 15:37:45 -0300

    feat: Exercício 3 feito

---
+++ Exercício3.js
@@ -0,0 +1,12 @@
+// Exercício 3: Ímpar ou par
+// Faça um programa que receba um número do usuário e informe se este número é par ou ímpar.
+
+let numero = prompt ("Digite um número")
+
+if ( numero%2 == 0) {
+    console.log ("Número é par")
+}
+
+else {
+    console.log("Número é ímpar")
+}
