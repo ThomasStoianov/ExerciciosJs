89b36d08283c152e77349468ae34065ef1cb2c82
Thomas-Stoianov <thomasgstoianov2@gmail.com>
2024-05-03 15:13:49 -0300

    feat: Exercício 2 feito

---
+++ Exercicio2.js
@@ -0,0 +1,31 @@
+let semana = prompt ("Digite um número de um a sete para descobrir que dia ele representa")
+
+if (semana == 1) {
+    console.log ("Segunda-feira")
+}
+
+if (semana == 2) {
+    console.log ("Terça-feira")
+}
+
+if (semana == 3) {
+    console.log ("Quarta-feira")
+}
+
+if (semana == 4) {
+    console.log ("Quinta-feira")
+}
+
+if(semana == 5) {
+    console.log ("Sexta-feira")
+}
+
+if (semana == 6 ) {
+    console.log ("Sábado")
+}
+
+if (semana == 7 ) {
+    console.log ("domingo")
+}
+
+else ("Esse número não é válido")
