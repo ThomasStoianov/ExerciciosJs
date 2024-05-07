8157bd68d70bf165899143e6557a75197710045b
ThomasStoianov <166552620+ThomasStoianov@users.noreply.github.com>
2024-05-07 17:26:07 -0300

    Update ex3.js

--- loop/ex3.js
+++ loop/ex3.js
@@ -1,4 +1,4 @@
-3. Faça um programa que imprima os números ímpares de 0 a 50;
+// 3. Faça um programa que imprima os números ímpares de 0 a 50;
 
 for (let cont = 1; cont <=50; cont+=2) {
     console.log (cont)
