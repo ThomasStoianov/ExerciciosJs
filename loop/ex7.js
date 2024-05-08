c772d42ee4b40a5abe7a96eafbd6039d2743084f
Thomas-Stoianov <thomasgstoianov2@gmail.com>
2024-05-08 15:34:35 -0300

    feat: exercicio 7 loop feito

---
+++ loop/ex7.js
@@ -0,0 +1,10 @@
+// Calcular a soma dos números de 1 a 100 utilizando a estrutura
+// while.
+
+let soma = 0
+let numero1 = 1
+
+while (numero1 <= 100) {
+    console.log(soma = soma + numero1)
+    numero1++;
+}
