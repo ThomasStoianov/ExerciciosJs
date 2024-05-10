b3badca78b14dc4013abd9d27c470d9d4cc83aea
Thomas-Stoianov <thomasgstoianov2@gmail.com>
2024-05-10 14:51:26 -0300

    Feat: exercicio Função.Menu feito

--- Função.Menu/index.js
+++ Função.Menu/index.js
@@ -17,7 +17,7 @@ function ex2 () {
 
 function ex5 () {
     num1 = 0
-num2 = 1
+    num2 = 1
 
 for (cont = 1; cont <= 10; cont++){
     result = num1 + num2
@@ -59,3 +59,34 @@ do {
 
 console.log("Voce digitou 0. O programa foi encerrado")
 }
+
+do {
+    option = parseInt(prompt("Digite uma das opções: \n 1 - Imprimir números de 1 a 10 utilizando o loop for\n2 - Imprimir números pares de 1 a 10.\n 3 - Imprimir os primeiros 10 números da sequência de Fibonacci.\n 4 -  Imprimir números pares de 0 a 20 utilizando o loop while\n 5 - Faça um programa que receba a idade do usuário, enquanto a resposta do usuário for menor que 18 anos continue a solicitar a idade e mostre uma mensagem quando a idade do usuário for maior que 18.\n 6 - Faça um programa que receba um número do usuário e continue a pedir o número se o numero for diferente de 0.\n Digite 0 para sair"))
+
+    switch (option) {
+        case 1:
+            ex1 ()
+            break
+
+    case 2:
+        ex2 ()
+        break
+        
+    case 3:
+        ex5 ()
+        break
+
+    case 4:
+        ex6 ()
+        break
+        
+    case 5:
+        ex8 ()
+        break
+        
+    case 6:
+        ex10 ()
+        break    
+    }
+
+}while(option !== 0)
